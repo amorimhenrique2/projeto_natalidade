ac5286ccaf2559facc09498de9faab66a14af3be
AMORIMHENRIQUE2 <amorimhenrique@id.uff.br>
2026-04-17 17:50:50 -0300

    lendo o banco de dados

--- projeto_5.R
+++ projeto_5.R
@@ -1,1 +1,10 @@
 #abrindo o arquivo
+
+library(readr)
+library(readxl)
+
+dados = read.csv("SINASC_2015.csv",header = TRUE,sep = ";")
+dim(dados)
+
+dados_sinacs <-  read.csv("SINASC_2015.csv",header = TRUE,sep = ";")
+dim(dados_sinacs)
