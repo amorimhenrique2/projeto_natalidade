cc86750f41a616d03d454f8c98e2099e69d47823
AMORIMHENRIQUE2 <amorimhenrique@id.uff.br>
2026-04-17 17:59:03 -0300

    tarefa 2 concluida

--- projeto_5.R
+++ projeto_5.R
@@ -8,3 +8,6 @@ dim(dados)
 
 dados_sinacs <-  read.csv("SINASC_2015.csv",header = TRUE,sep = ";")
 dim(dados_sinacs)
+str(dados_sinacs)
+
+dados_sinacs_1 <- dados_sinacs[, c(1, 4, 5, 6, 7, 12, 13, 14, 15, 19, 21, 22, 23, 24, 35, 38, 44, 46, 48, 59, 60, 61)] 
