c114fb6b7387bfc9e249d41ccd480a648e3fbb17
AMORIMHENRIQUE2 <amorimhenrique@id.uff.br>
2026-05-02 15:29:33 -0300

    correção da tarefa 1 - 7 na branch sinasc

--- projeto_5.R
+++ projeto_5.R
@@ -4,21 +4,53 @@
 library(readr)
 library(readxl)
 
-dados = read.csv("SINASC_2015.csv",header = TRUE,sep = ";")
-dim(dados)
 
-dados_sinasc <-  read.csv("SINASC_2015.csv",header = TRUE,sep = ";")
-dim(dados_sinasc)
+dados_sinasc = read.csv("SINASC_2015.csv", sep=";", header=TRUE)
+head(dados_sinasc)
 str(dados_sinasc)
+# -------------------------------
+# Tarefa 2: Seleção de colunas
+# -------------------------------
+dados_sinasc_1 = dados_sinasc[, c(1, 4, 5, 6, 7, 12, 13, 14, 15, 19, 21, 22, 23, 24, 35, 38, 44, 46, 48, 59, 60, 61)]
+summary(dados_sinasc_1)
 
-#tarefa 2
-dados_sinasc_1 <- dados_sinasc[, c(1, 4, 5, 6, 7, 12, 13, 14, 15, 19, 21, 22, 23, 24, 35, 38, 44, 46, 48, 59, 60, 61)] 
-dados_sinasc_1$CODMUNRES <- as.character(dados_sinasc_1$CODMUNRES)
 
-#tarefa 3
-dados_sinasc_2 <- dados_sinasc_1[substr(dados_sinasc_1$CODMUNRES, 1, 2) == "23", ]
+summary(dados_sinasc_1$CODMUNRES)
+UF = substr(as.character(dados_sinasc_1$CODMUNRES), 1, 2)
 
-#tarefa 4
+
+# -------------------------------
+# Tarefa 3: Filtrar CEARÁ (23)
+# -------------------------------
+
+dados_sinasc_2 = dados_sinasc_1[UF == "23", ]
+
+write.csv(dados_sinasc_2, "dados_sinasc_2.csv", row.names = FALSE)
+
+rm(dados_sinasc, dados_sinasc_1)
+gc()
+
+
+# -------------------------------
+# Tarefa 4: Frequências
+# -------------------------------
+
+freq_LOCNASC <- table(dados_sinasc_2$LOCNASC)
+freq_ESTCIVMAE <- table(dados_sinasc_2$ESTCIVMAE)
+freq_GESTACAO <- table(dados_sinasc_2$GESTACAO)
+freq_GRAVIDEZ <- table(dados_sinasc_2$GRAVIDEZ)
+freq_PARTO <- table(dados_sinasc_2$PARTO)
+freq_SEXO <- table(dados_sinasc_2$SEXO)
+freq_APGAR5 <- table(dados_sinasc_2$APGAR5)
+freq_RACACOR <- table(dados_sinasc_2$RACACOR)
+freq_IDANOMAL <- table(dados_sinasc_2$IDANOMAL)
+freq_ESCMAE2010 <- table(dados_sinasc_2$ESCMAE2010)
+freq_RACACORMAE <- table(dados_sinasc_2$RACACORMAE)
+freq_TPAPRESENT <- table(dados_sinasc_2$TPAPRESENT)
+freq_TPROBSON <- table(dados_sinasc_2$TPROBSON)
+freq_PARIDADE <- table(dados_sinasc_2$PARIDADE)
+freq_KOTELCHUCK <- table(dados_sinasc_2$KOTELCHUCK)
+
 
 table(dados_sinasc_2$LOCNASC)
 table(dados_sinasc_2$ESTCIVMAE)
@@ -26,7 +58,6 @@ table(dados_sinasc_2$GESTACAO)
 table(dados_sinasc_2$GRAVIDEZ)
 table(dados_sinasc_2$PARTO)
 table(dados_sinasc_2$SEXO)
-table(dados_sinasc_2$APGAR5)
 table(dados_sinasc_2$RACACOR)
 table(dados_sinasc_2$IDANOMAL)
 table(dados_sinasc_2$ESCMAE2010)
@@ -37,95 +68,93 @@ table(dados_sinasc_2$PARIDADE)
 table(dados_sinasc_2$KOTELCHUCK)
 
 
-#TAREFA 5
-dados_sinasc_2$SEXO[dados_sinasc_2$SEXO == 0] <- NA
-dados_sinasc_2$APGAR5[dados_sinasc_2$APGAR5 == 99] <- NA
-dados_sinasc_2$RACACOR[dados_sinasc_2$RACACOR == 9] <- NA
 
-dados_sinasc_2[dados_sinasc_2 == "Ignorado"] <- NA
-dados_sinasc_2[dados_sinasc_2 == "Nao informado"] <- NA
-
-
-
-#tarefa 6 
-# SEXO
-dados_sinasc_2$SEXO <- factor(
-  dados_sinasc_2$SEXO,
-  levels = c(1,2),
-  labels = c("Masculino", "Feminino")
-)
-
-# PARTO
-dados_sinasc_2$PARTO <- factor(
-  dados_sinasc_2$PARTO,
-  levels = c(1,2),
-  labels = c("Vaginal", "Cesáreo")
-)
-
-# GRAVIDEZ
-dados_sinasc_2$GRAVIDEZ <- factor(
-  dados_sinasc_2$GRAVIDEZ,
-  levels = c(1,2,3),
-  labels = c("Única", "Dupla", "Tripla ou mais")
-)
-
-# ESTADO CIVIL
-dados_sinasc_2$ESTCIVMAE <- factor(
-  dados_sinasc_2$ESTCIVMAE,
-  levels = c(1,2,3,4,5),
-  labels = c("Solteira", "Casada", "Viúva", "Separada", "União estável")
-)
-
-# KOTELCHUCK
-dados_sinasc_2$KOTELCHUCK <- factor(
-  dados_sinasc_2$KOTELCHUCK,
-  levels = c(1,2,3,4,5),
-  labels = c("Não realizou pré-natal", "Inadequado", "Intermediário", "Adequado", "Mais que adequado")
-)
-#tarefa 7
-
-dados_sinasc_2$IDADEMAE <- as.numeric(dados_sinasc_2$IDADEMAE)
-dados_sinasc_2$PESO <- as.numeric(dados_sinasc_2$PESO)
-dados_sinasc_2$APGAR5 <- as.numeric(dados_sinasc_2$APGAR5)
-
-# PESO
-dados_sinasc_2$F_PESO <- NA
-dados_sinasc_2$F_PESO[dados_sinasc_2$PESO < 2500] <- "Baixo peso"
-dados_sinasc_2$F_PESO[dados_sinasc_2$PESO >= 2500 & dados_sinasc_2$PESO < 4000] <- "Peso normal"
-dados_sinasc_2$F_PESO[dados_sinasc_2$PESO >= 4000] <- "Macrossomia"
-dados_sinasc_2$F_PESO <- factor(dados_sinasc_2$F_PESO)
-
-# IDADE
-dados_sinasc_2$F_IDADE <- NA
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE < 15] <- "<15"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 15 & dados_sinasc_2$IDADEMAE <= 19] <- "15-19"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 20 & dados_sinasc_2$IDADEMAE <= 24] <- "20-24"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 25 & dados_sinasc_2$IDADEMAE <= 29] <- "25-29"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 30 & dados_sinasc_2$IDADEMAE <= 34] <- "30-34"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 35 & dados_sinasc_2$IDADEMAE <= 39] <- "35-39"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 40 & dados_sinasc_2$IDADEMAE <= 44] <- "40-44"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 45 & dados_sinasc_2$IDADEMAE <= 49] <- "45-49"
-dados_sinasc_2$F_IDADE[dados_sinasc_2$IDADEMAE >= 50] <- "50+"
-dados_sinasc_2$F_IDADE <- factor(dados_sinasc_2$F_IDADE)
-
-# APGAR5
-dados_sinasc_2$F_APGAR5 <- NA
-dados_sinasc_2$F_APGAR5[dados_sinasc_2$APGAR5 < 7] <- "Baixo"
-dados_sinasc_2$F_APGAR5[dados_sinasc_2$APGAR5 >= 7] <- "Normal"
-dados_sinasc_2$F_APGAR5 <- factor(dados_sinasc_2$F_APGAR5)
-
-# PEREGRINAÇÃO
-dados_sinasc_2$PERIG <- NA
-dados_sinasc_2$PERIG[dados_sinasc_2$CODMUNNASC == dados_sinasc_2$CODMUNRES] <- "Não"
-dados_sinasc_2$PERIG[dados_sinasc_2$CODMUNNASC != dados_sinasc_2$CODMUNRES] <- "Sim"
-dados_sinasc_2$PERIG <- factor(dados_sinasc_2$PERIG)
-
-# ESTADO CIVIL (agrupado)
-dados_sinasc_2$ESTCIV <- NA
-dados_sinasc_2$ESTCIV[dados_sinasc_2$ESTCIVMAE %in% c(1,3,4)] <- "Sem companheiro"
-dados_sinasc_2$ESTCIV[dados_sinasc_2$ESTCIVMAE %in% c(2,5)] <- "Com companheiro"
-dados_sinasc_2$ESTCIV <- factor(dados_sinasc_2$ESTCIV)
+# Aproveitando para ver os valores das variáveis quantitativas
+unique(dados_sinasc_2$IDADEMAE)
+unique(dados_sinasc_2$CONSPRENAT)
+unique(dados_sinasc_2$SEMAGESTAC)
+unique(dados_sinasc_2$APGAR5)
+unique(dados_sinasc_2$PESO)
+summary(dados_sinasc_2$PESO)
 
 
+# -------------------------------
+# Tarefa 5: Tratar NA
+# -------------------------------
+dados_sinasc_2$LOCNASC[dados_sinasc_2$LOCNASC == 9] = NA
+dados_sinasc_2$IDADEMAE[dados_sinasc_2$IDADEMAE == 99] = NA
+dados_sinasc_2$ESTCIVMAE[dados_sinasc_2$ESTCIVMAE == 9] = NA
+dados_sinasc_2$GESTACAO[dados_sinasc_2$GESTACAO == 9] = NA
+dados_sinasc_2$GRAVIDEZ[dados_sinasc_2$GRAVIDEZ == 9] = NA
+dados_sinasc_2$PARTO[dados_sinasc_2$PARTO == 9] = NA
+dados_sinasc_2$SEXO[dados_sinasc_2$SEXO == 0] = NA
+dados_sinasc_2$APGAR5[dados_sinasc_2$APGAR5 == 99] = NA
+dados_sinasc_2$PESO[dados_sinasc_2$PESO == 9999] = NA
+dados_sinasc_2$IDANOMAL[dados_sinasc_2$IDANOMAL == 9] = NA
+dados_sinasc_2$ESCMAE2010[dados_sinasc_2$ESCMAE2010 == 9] = NA
+dados_sinasc_2$CONSPRENAT[dados_sinasc_2$CONSPRENAT == 99] = NA
+dados_sinasc_2$TPAPRESENT[dados_sinasc_2$TPAPRESENT == 9] = NA
+dados_sinasc_2$TPROBSON[dados_sinasc_2$TPROBSON == 11] = NA
+dados_sinasc_2$KOTELCHUCK[dados_sinasc_2$KOTELCHUCK == 9] = NA
+summary(dados_sinasc_2)
 
+
+n_total_nasc_UF = nrow(dados_sinasc_2)
+n_total_nasc_UF_sem_missing = sum(complete.cases(dados_sinasc_2))
+n_total_nasc_MUN = tapply(rep(1, nrow(dados_sinasc_2)), dados_sinasc_2$CODMUNRES, sum)
+n_total_nasc_MUN_sem_missing = tapply(complete.cases(dados_sinasc_2), dados_sinasc_2$CODMUNRES, sum)
+
+
+# -------------------------------
+# Tarefa 6: Labels (exemplo)
+# -------------------------------
+
+
+dados_sinasc_2$LOCNASC = factor(dados_sinasc_2$LOCNASC, levels = c(1,2,3,4), labels = c("Hospital", "Outros estabelecimentos de saúde", "Domicílio", "Outros"))
+dados_sinasc_2$ESTCIVMAE = factor(dados_sinasc_2$ESTCIVMAE, levels = c(1,2,3,4,5), labels = c("Solteira", "Casada", "Viúva", "Separada judicialmente/divorciada", "União estável"))
+dados_sinasc_2$GESTACAO = factor(dados_sinasc_2$GESTACAO, levels = c(1,2,3,4,5,6), labels = c("Menos de 22 semanas", "22 a 27 semanas", "28 a 31 semanas", "32 a 36 semanas", "32 a 36 semanas", "42 semanas e mais"))
+dados_sinasc_2$GRAVIDEZ = factor(dados_sinasc_2$GRAVIDEZ, levels = c(1,2,3), labels = c("Única", "Dupla", "Tripla ou mais"))
+dados_sinasc_2$PARTO = factor(dados_sinasc_2$PARTO, levels = c(1,2), labels = c("Vaginal", "Cesário"))
+dados_sinasc_2$SEXO = factor(dados_sinasc_2$SEXO, levels = c(1,2), labels = c("Masculino", "Feminino"))
+dados_sinasc_2$RACACOR = factor(dados_sinasc_2$RACACOR, levels = c(1,2,3,4,5), labels = c("Branca", "Preta", "Amarela", "Parda", "Indígena"))
+dados_sinasc_2$IDANOMAL = factor(dados_sinasc_2$IDANOMAL, levels = c(1,2), labels = c("Sim", "Não"))
+dados_sinasc_2$ESCMAE2010 = factor(dados_sinasc_2$ESCMAE2010, levels = c(0,1,2,3,4,5), labels = c("Sem escolaridade", "Fundamental I (1ª a 4ª série)", "Fundamental II (5ª a 8ª série)", "Médio (antigo 2º grau)", "Superior incompleto", "Superior completo"))
+dados_sinasc_2$RACACORMAE = factor(dados_sinasc_2$RACACORMAE, levels = c(1,2,3,4,5), labels = c("Branca", "Preta", "Amarela", "Parda", "Indígena"))
+dados_sinasc_2$TPAPRESENT = factor(dados_sinasc_2$TPAPRESENT, levels = c(1,2,3), labels = c("Cefálico", "Pélvica ou podálica", "Transversa"))
+dados_sinasc_2$TPROBSON = factor(dados_sinasc_2$TPROBSON, levels = c(1,2,3,4,5,6,7,8,9,10), labels = c("Grupo 1", "Grupo 2", "Grupo 3", "Grupo 4", "Grupo 5", "Grupo 6", "Grupo 7", "Grupo 8", "Grupo 9", "Grupo 10"))
+dados_sinasc_2$PARIDADE = factor(dados_sinasc_2$PARIDADE, levels = c(0,1), labels = c("Nulípara", "Multípara"))
+dados_sinasc_2$KOTELCHUCK = factor(dados_sinasc_2$KOTELCHUCK, levels = c(1,2,3,4,5), labels = c("Não realizou pré-natal", "Inadequado", "Intermediário", "Adequado", "Mais que adequado"))
+
+
+# -------------------------------
+# Tarefa 7: Novas variáveis
+# -------------------------------
+
+dados_sinasc_2$F_IDADE = ifelse(dados_sinasc_2$IDADEMAE < 15, "<15",
+                                ifelse(dados_sinasc_2$IDADEMAE <= 19, "15-19",
+                                       ifelse(dados_sinasc_2$IDADEMAE <= 24, "20-24",
+                                              ifelse(dados_sinasc_2$IDADEMAE <= 29, "25-29",
+                                                     ifelse(dados_sinasc_2$IDADEMAE <= 34, "30-34",
+                                                            ifelse(dados_sinasc_2$IDADEMAE <= 39, "35-39",
+                                                                   ifelse(dados_sinasc_2$IDADEMAE <= 44, "40-44",
+                                                                          ifelse(dados_sinasc_2$IDADEMAE <= 49, "45-49",
+                                                                                 "50+"))))))))
+dados_sinasc_2$F_IDADE = factor(dados_sinasc_2$F_IDADE,
+                                levels = c("<15","15-19","20-24","25-29","30-34","35-39","40-44","45-49","50+"), ordered = TRUE)
+
+dados_sinasc_2$F_PESO = ifelse(dados_sinasc_2$PESO < 2500, "Baixo peso",
+                               ifelse(dados_sinasc_2$PESO < 4000, "Peso normal",
+                                      "Macrossomia"))
+dados_sinasc_2$F_PESO = factor(dados_sinasc_2$F_PESO, levels = c("Baixo peso","Peso normal","Macrossomia"))
+
+dados_sinasc_2$F_APGAR5 = ifelse(dados_sinasc_2$APGAR5 < 7, "Baixo", "Normal")
+dados_sinasc_2$F_APGAR5 = factor(dados_sinasc_2$F_APGAR5,levels = c("Baixo","Normal"))
+
+dados_sinasc_2$PERIG = ifelse(is.na(dados_sinasc_2$CODMUNNASC) | is.na(dados_sinasc_2$CODMUNRES), NA,
+                              ifelse(dados_sinasc_2$CODMUNNASC == dados_sinasc_2$CODMUNRES, "Não", "Sim"))
+dados_sinasc_2$PERIG = factor(dados_sinasc_2$PERIG, levels = c("Não", "Sim"))
+
+dados_sinasc_2$ESTCIV = ifelse(dados_sinasc_2$ESTCIVMAE %in% c("Solteira", "Viúva", "Separada judicialmente/divorciada"), "Sem companheiro",
+                               ifelse(dados_sinasc_2$ESTCIVMAE %in% c("Casada", "União estável"), "Com companheiro", NA))
+dados_sinasc_2$ESTCIV = factor(dados_sinasc_2$ESTCIV, levels = c("Sem companheiro","Com companheiro"))
 
